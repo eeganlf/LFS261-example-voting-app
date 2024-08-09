c42f4958f71b30c71f93d73a2eab0ef257e0ae2d
eeganlf <101604439+eeganlf@users.noreply.github.com>
2024-08-09 12:14:56 -0700

    Update app.py
    Signed-off-by: eeganlf <101604439+eeganlf@users.noreply.github.com>

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Chocolate")
+option_b = os.getenv('OPTION_B', "Vanilla")
 hostname = socket.gethostname()
 version = 'v1'
 
